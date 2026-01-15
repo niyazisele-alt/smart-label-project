815d26e355ad4159c8377f2fe34884e805c0966e
niyazisele-alt <niyazisele@gmail.com>
2026-01-15 18:38:15 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,29 @@
-
 from flask import Flask, jsonify
+from led import show_label as led_show
+from lcd import show_label as lcd_show
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    # Ürün verisi
+    product = {
+        "name": "Süt 1L",
+        "price": 29.90,
+        "barcode": "869000000001"
+    }
+
+    # LED ekran simülasyonu
+    led_show(product)
+
+    # LCD ekran simülasyonu
+    lcd_show(product)
+
+    # Tarayıcıda JSON olarak göster
     return jsonify({
         "project": "Smart Label",
         "status": "running",
+        "product": product,
         "display": ["LED", "LCD"]
     })
 
